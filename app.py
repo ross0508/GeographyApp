0d0420b24567801c7c24f04c396b050561c06168
ross0508 <rossdavidson02@gmail.com>
2026-01-01 19:33:07 +0000

    Add login and register endpoints

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, jsonify, request
-from sqlalchemy import Column, Integer, Table, Column, MetaData, String, Double, ForeignKey
+from sqlalchemy import Column, Integer, Table, Column, MetaData, String, Double, ForeignKey, LargeBinary
 from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 from engine import engine
 import json
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token, JWTManager, get_jwt_identity, jwt_required
+import bcrypt
 
 app = Flask(__name__)
+app.config["JWT_SECRET_KEY"] = "sleepyjoe"
+
 CORS(app)
+
+jwt = JWTManager(app)
 
 connection = engine.connect()
 
@@ -25,14 +31,14 @@ class User(Base):
     __tablename__ = 'users'
 
     user_id = Column(Integer, primary_key=True)
-    name = Column(String)
-    password_hash = Column(Integer)
+    username = Column(String)
+    password_hash = Column(LargeBinary)
     exp = Column(Integer)
     level = Column(Integer)
     exp_to_next_level = Column(Integer)
 
     
-    facts = relationship('facts', secondary='user_fact', back_populates='users')
+    facts = relationship('Fact', secondary='user_fact', back_populates='users')
 
 class Fact(Base):
     __tablename__ = 'facts'
@@ -43,7 +49,7 @@ class Fact(Base):
     Img_url = Column(String)
     Answer = Column(String)
 
-    users = relationship('users', secondary="user_fact", back_populates="facts")
+    users = relationship('User', secondary="user_fact", back_populates="facts")
 
 
 Base.metadata.create_all(engine)
@@ -51,3 +57,42 @@ Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 
 session = Session()
+
+@app.post("/register")
+def create_account():
+    data = request.get_json()
+    username = data["username"]
+    password = data["password"]
+
+    password_hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+    user = User(username=username, password_hash=password_hash, exp=0, level=1, exp_to_next_level=100)
+
+    session.add(user)
+
+    session.commit()
+
+    return {"bruh": "bruh"}
+
+
+@app.post("/login")
+def create_token():
+    username = request.json.get("username")
+
+    password = request.json.get("password")
+
+    user = session.query(User).filter_by(username=username).one_or_none()
+    
+    if user:
+        login_successful = bcrypt.checkpw(password.encode('utf-8'), user.password_hash)
+        if login_successful:
+            token = create_access_token(identity=username)
+            return {username: token}
+
+    return {"Error": "Incorrect username or password"}
+   
+
+@app.post("/test")
+@jwt_required()
+def test():
+    return {"bruh": "bruvvvvvv"}
