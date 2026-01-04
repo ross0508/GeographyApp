b177cbcb925dd77b803a9a02399fba48891e6186
ross0508 <rossdavidson02@gmail.com>
2026-01-04 18:24:03 +0000

    Add routing

--- frontend/geographyapp/src/App.tsx
+++ frontend/geographyapp/src/App.tsx
@@ -1,105 +1,17 @@
-import { useState } from "react";
 import "./App.css";
-import axios from "axios";
-import Cookies from "universal-cookie";
+import { Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import Profile from "./Profile";
+import Quiz from "./Quiz";
 
 function App() {
-  const cookies = new Cookies();
-
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleLogin = async () => {
-    event.preventDefault();
-    try {
-      const response = await axios({
-        method: "POST",
-        url: `http://127.0.0.1:5000/login`,
-        data: {
-          username: username,
-          password: password,
-        },
-      });
-      cookies.set("jwt_authorization", response.data["token"]);
-      return response.data;
-    } catch (error) {
-      console.error("Error logging in", error);
-    }
-  };
-
-  const handleRegister = async () => {
-    event.preventDefault();
-    try {
-      const response = await axios({
-        method: "POST",
-        url: `http://localhost:5000/register`,
-        data: {
-          username: username,
-          password: password,
-        },
-      });
-      return response.data;
-    } catch (error) {
-      console.error("Error signing up", error);
-    }
-  };
-
-  const testRequest = async () => {
-    event.preventDefault();
-    const jwt_authorization = cookies.get("jwt_authorization");
-    const auth_header = "Bearer " + jwt_authorization;
-    try {
-      const response = await axios({
-        method: "POST",
-        url: `http://localhost:5000/test`,
-        headers: {
-          Authorization: auth_header,
-        },
-        data: {
-          username: username,
-          password: password,
-        },
-      });
-      return response.data;
-    } catch (error) {
-      console.error("Error signing up", error);
-    }
-  };
-
   return (
     <>
-      <div className="card">
-        <form className="login-form">
-          <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input
-              type="username"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              placeholder="Enter your username"
-            />
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Enter your password"
-            />
-          </div>
-          <button onClick={() => handleLogin()} className="login-button">
-            Login
-          </button>
-          <button onClick={() => handleRegister()} className="login-button">
-            Register
-          </button>
-          <button onClick={() => testRequest()}>Test Request</button>
-        </form>
-      </div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </>
   );
 }
