ed86d06f8c2a268cebb89fcc03ce2cf62fced2ba
ross0508 <rossdavidson02@gmail.com>
2026-01-06 22:23:40 +0000

    Show friends list

--- frontend/geographyapp/src/Social.tsx
+++ frontend/geographyapp/src/Social.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import AddFriend from "./AddFriend";
 import NavBar from "./Navbar";
 import Cookies from "universal-cookie";
@@ -7,8 +7,30 @@ import axios from "axios";
 export default function Social() {
   const cookies = new Cookies();
 
-  const [friendRequests, setFriendRequests] = React.useState([]);
-  const [dataGotten, setDataGotten] = React.useState(false);
+  const [friendRequests, setFriendRequests] = useState([]);
+  const [requestDataGotten, setRequestDataGotten] = useState(false);
+  const [friends, setFriends] = useState([]);
+  const [friendDataGotten, setFriendDataGotten] = useState(false);
+
+  const getFriends = async () => {
+    const jwt_authorization = cookies.get("jwt_authorization");
+    const auth_header = "Bearer " + jwt_authorization;
+    try {
+      const response = await axios({
+        method: "GET",
+        url: `http://127.0.0.1:5000/friends`,
+        headers: {
+          Authorization: auth_header,
+        },
+      });
+      setFriends(response.data);
+      console.log("Friends fetched:", response.data);
+      setFriendDataGotten(true);
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching friends from backend:", error.response);
+    }
+  };
 
   const getFriendRequests = async () => {
     const jwt_authorization = cookies.get("jwt_authorization");
@@ -23,10 +45,13 @@ export default function Social() {
       });
       setFriendRequests(response.data);
       console.log("Friend requests fetched:", response.data);
-      setDataGotten(true);
+      setRequestDataGotten(true);
       return response.data;
     } catch (error) {
-      console.error("Error fetching data from backend:", error.response);
+      console.error(
+        "Error fetching friend requests from backend:",
+        error.response
+      );
     }
   };
 
@@ -34,6 +59,9 @@ export default function Social() {
     setFriendRequests((prevRequests) =>
       prevRequests.filter((req) => req.request_id !== request_id)
     );
+    if (accept) {
+      setFriends((prevFriends) => prevFriends.filter((friend) => friend));
+    }
     const jwt_authorization = cookies.get("jwt_authorization");
     const auth_header = "Bearer " + jwt_authorization;
     try {
@@ -53,21 +81,26 @@ export default function Social() {
     }
   };
 
-  React.useEffect(() => {
-    if (!dataGotten) {
-      console.log("Fetching friend requests...");
+  useEffect(() => {
+    if (!requestDataGotten) {
       getFriendRequests();
     }
-  }, [dataGotten]);
+  }, [requestDataGotten]);
+
+  useEffect(() => {
+    if (!friendDataGotten) {
+      getFriends();
+    }
+  }, [friendDataGotten]);
 
   return (
     <div>
       <NavBar></NavBar>
       <AddFriend />
-      {dataGotten && friendRequests.length === 0 && (
+      {requestDataGotten && friendRequests.length === 0 && (
         <p>No pending friend requests.</p>
       )}
-      {dataGotten && friendRequests.length > 0 && (
+      {requestDataGotten && friendRequests.length > 0 && (
         <div>
           <h3>Pending Friend Requests:</h3>
           <ul>
@@ -86,6 +119,18 @@ export default function Social() {
                 >
                   ✗
                 </button>
+              </div>
+            ))}
+          </ul>
+        </div>
+      )}
+      {friendDataGotten && friends.length > 0 && (
+        <div>
+          <h3>Friends:</h3>
+          <ul>
+            {friends.map((friend, index) => (
+              <div key={index}>
+                <li>{friend}</li>
               </div>
             ))}
           </ul>
